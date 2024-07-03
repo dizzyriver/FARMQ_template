e6f28d4a8e6c5801c8e8faa39ec18a89743d9042
Vasilis Hatzopoulos <vas.h@protonmail.com>
2024-07-03 12:41:18 +0300

    fix store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [thunk],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 export default store;
