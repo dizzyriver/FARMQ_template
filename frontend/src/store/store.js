ff0c5f8bb3aea74be3816fe7162e66378cad8172
Vasilis Hatzopoulos <vas.h@protonmail.com>
2024-07-03 12:16:55 +0300

    remove redux-extensions

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+});
 
 export default store;
